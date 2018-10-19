473c30f7b26ebf748fdf8ae46c311da019393ee7
kkdp13 <41451325+kkdp13@users.noreply.github.com>
2018-10-20 00:02:20 +0700

    Add files via upload
    update juti vdo

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ version 1.00
 from flask import Flask, request
 import json
 import requests
-from googlesheet import getstockinfo
-from breakr1 import breakr1
+from eyam import eyam
+from googlesheet import eyaminfo, modeupdate, ymodeupdate, highupdate, lowupdate, resetnewday
 
 # ตรง YOURSECRETKEY ต้องนำมาใส่เองครับจะกล่าวถึงในขั้นตอนต่อๆ ไป
 global LINE_API_KEY
 # ห้ามลบคำว่า Bearer ออกนะครับเมื่อนำ access token มาใส่
-LINE_API_KEY = 'Bearer pi/PPBybqenSQhskt2bdBpV+OXZTiA1SR96ef++xlwEGgILVvbZ+gqgnZto6woGcMTg85fHs4K+vTv4sCyDvFRxOu/F7h7rhrKrVP1PlCruWEga2G2nX+T/s5ntyC1lQDIbma7+QZGhCZsue/+tkggdB04t89/1O/w1cDnyilFU='
+LINE_API_KEY = 'Bearer ni2OchQdVILm0CxVI4fUHmfMIzPaYMtxuHIpQoKH1BJLOlKzgoV3t/z9VQCH95N+Oawt40F4qVxbnovStWXBcZzH783TOAoB6I6k5d6t6qc/EiFukdIydJX4JL3xn3XZ6TXLV8uh3jgksHsh2rMvyQdB04t89/1O/w1cDnyilFU='
 
 app = Flask(__name__)
  
@@ -59,50 +59,46 @@ def bot():
     text = msg_in_json["events"][0]['message']['text'].lower().strip()
     textstart = text[0]
     
-    # if textstart == '/':
-    #     if text[1] == 'b':
-    #         stocklistr1 = breakr1()
-    #         number = len(stocklistr1)
-    #         numbertext = "stock list r1 {}".format(number)
-    #         replyQueue.append(numbertext)
-    #         reply(replyToken, replyQueue[:5])
-    #     elif text[1] == 'y':
-    #         yesterdaymode = text.split(",")[1]
-    #         ymodeupdate(yesterdaymode)
-    #         replyQueue.append("ymode updated : {}".format(yesterdaymode))
-    #         reply(replyToken, replyQueue[:5])
-    #     elif text[1] == 'h':
-    #         todayHIGH = text.split(",")[1]
-    #         highupdate(todayHIGH)
-    #         replyQueue.append("HIGH updated : {}".format(todayHIGH))
-    #         reply(replyToken, replyQueue[:5])
-    #     elif text[1] == 'l':
-    #         todayLOW = text.split(",")[1]
-    #         lowupdate(todayLOW)
-    #         replyQueue.append("LOW updated : {}".format(todayLOW))
-    #         reply(replyToken, replyQueue[:5])
-    #     elif text[1] == 'r':
-    #         resetdone = resetnewday()
-    #         if resetdone > 0:
-    #             replyQueue.append("reset")
-    #             reply(replyToken, replyQueue[:5])
-    #         else:
-    #             replyQueue.append("กด reset ไปแล้วครับ")
-    #             reply(replyToken, replyQueue[:5])   
-        # return 'OK', 200
-    if textstart == '=':
-        stockinfo = getstockinfo(text[1:])
-        """stockname,stocklow,stocks1,stocksmode1,stocks2,stocksmode2,
-        stocks3,stocksmode3,stockr1,stockrmode1,stockr2,stockrmode2,
-        stockr3,stockrmode3,stockr4,stockrmode4,stockr5,stockrmode5,
-        stocklastprice,stockbreaks1,stockbreaks2,stockbreaks3,
-        stockbreakr1,stockbreakr2,stockbreakr3,stockbreakr4,
-        stockbreakr5,stockrunentry,stockrunreward,stockrunrisk,stockrunrr,
-        stocknewentry,stocknewreward,stocknewrisk,stocknewrr"""
-        info = "Stock Name = {}\nStock Low = {}\nStock S1 = {}\nMode Range = {}\nStock S2 = {}\nMode Range = {}\nStock S3 = {}\nMode Range = {}\n".format(stockinfo[0],stockinfo[1],stockinfo[2],stockinfo[3],stockinfo[4],stockinfo[5],stockinfo[6],stockinfo[7])
-        info2 = "Stock R1 = {}\nMode Range = {}\nStock R2 = {}\nMode Range = {}\nStock R3 = {}\nMode Range = {}\nStock R4 = {}\nMode Range = {}\nStock R5 = {}\nMode Range = {}\n".format(stockinfo[8],stockinfo[9],stockinfo[10],stockinfo[11],stockinfo[12],stockinfo[13],stockinfo[14],stockinfo[15],stockinfo[16],stockinfo[17])
-        info3 = "Stock Last Price = {}\nStock Break S1 = {}\nStock Break S2 = {}\nStock Break S3 = {}\nStock Break R1 = {}\nStock Break R2 = {}\nStock Break R3 = {}\nStock Break R4 = {}\nStock Break R5 = {}\n".format(stockinfo[18],stockinfo[19],stockinfo[20],stockinfo[21],stockinfo[22],stockinfo[23],stockinfo[24],stockinfo[25],stockinfo[26])
-        info4 = "Stock Entry (Run) = {}\nStock Reward (Run) = {}\nStock Risk (Run) = {}\nStock RR (Run) = {}\nStock Entry (New) = {}\nStock Reward (New) = {}\nStock Risk (New) = {}\nStock RR (New) = {}".format(stockinfo[27],stockinfo[28],stockinfo[29],stockinfo[30],stockinfo[31],stockinfo[32],stockinfo[33],stockinfo[34])
+    if textstart == '/':
+        if text[1] == 't':
+            todaymode = text.split(",")[1]
+            modeupdate(todaymode)
+            replyQueue.append("tmode updated : {}".format(todaymode))
+            reply(replyToken, replyQueue[:5])
+        elif text[1] == 'y':
+            yesterdaymode = text.split(",")[1]
+            ymodeupdate(yesterdaymode)
+            replyQueue.append("ymode updated : {}".format(yesterdaymode))
+            reply(replyToken, replyQueue[:5])
+        elif text[1] == 'h':
+            todayHIGH = text.split(",")[1]
+            highupdate(todayHIGH)
+            replyQueue.append("HIGH updated : {}".format(todayHIGH))
+            reply(replyToken, replyQueue[:5])
+        elif text[1] == 'l':
+            todayLOW = text.split(",")[1]
+            lowupdate(todayLOW)
+            replyQueue.append("LOW updated : {}".format(todayLOW))
+            reply(replyToken, replyQueue[:5])
+        elif text[1] == 'r':
+            resetdone = resetnewday()
+            if resetdone > 0:
+                replyQueue.append("reset")
+                reply(replyToken, replyQueue[:5])
+            else:
+                replyQueue.append("กด reset ไปแล้วครับ")
+                reply(replyToken, replyQueue[:5])   
+        return 'OK', 200
+    elif textstart == '=':
+        geteyaminfos = eyaminfo()
+        """yesterdaymode,todaymode,todaytrend,
+        todayLY,todayJP,todayNN1,todayNN2,
+        todayKM1,todayKM2,todayLOW,todayHIGH,todaySet0,
+        until,KT,resis,support"""
+        info = "ymode = {}\ntmode = {}\ntrend = {}\nset0 = {}\n".format(geteyaminfos[0],geteyaminfos[1],geteyaminfos[2],geteyaminfos[11])
+        info2 = "LY 50% = {}\nJP 127% = {}\nNN1 161.8% = {}\nNN2 261.8% = {}\n".format(geteyaminfos[3],geteyaminfos[4],geteyaminfos[5],geteyaminfos[6])
+        info3 = "KM1 423.6% = {}\nKM2 685.4% = {}\nLOW = {}\nHIGH = {}\n".format(geteyaminfos[7],geteyaminfos[8],geteyaminfos[9],geteyaminfos[10])
+        info4 = "Till = {}\nKT = {}\nแนวต้าน = {},{},{}\nแนวรับ = {},{},{}\n".format(geteyaminfos[12],geteyaminfos[13],geteyaminfos[14],geteyaminfos[15],geteyaminfos[16],geteyaminfos[17],geteyaminfos[18],geteyaminfos[19])
         replyQueue.append(info+info2+info3+info4)
         reply(replyToken, replyQueue[:5])
         return 'OK', 200
